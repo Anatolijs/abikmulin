bde3f5f9be29f75eb3964f2b53f05c99e721d67c
Anatolijs <vpl@inbox.lv>
2018-01-11 14:29:00 +0200

    1.3. JavaDoc.Code convention[#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java/Calculate.java
@@ -1,7 +1,19 @@
 package ru.job4j;
 
 
+
+/**
+
+* Класс для арифметических операций.
+*/
+
+
 public class Calculate {
+
+
+/**
+* Конструктор для вывода строки в консоль.
+*/
 
    public static void main (String args[]) {
 
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,9 @@
+ 
+/**
+* Package for calculate task.
+*
+* @author Anatolik (vpl@inbox.lv)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
